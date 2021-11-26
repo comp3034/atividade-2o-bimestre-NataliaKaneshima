49a67edf6ed38fb48a83cda9d27128469a53e728
unknown <natalia.kaneshima@edu.unifil.br>
2021-11-26 00:39:59 -0300

    teste

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -18,6 +18,9 @@ def get_db():
     finally:
         db.close()
 
+#Aqui para baixo Usuario
+
+#cadastrar um usuario
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
@@ -28,7 +31,27 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     
     return user
 
+
+#entrega uma lista de usuarios
 @app.get("/users/", response_model=List[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+
+#Aqui para baixo Measure
+
+#cadastrar um usuario
+@app.post("/users/{user_id}/measures/", response_model=schemas.Measure)
+def create_measure(user_id: int, measure: schemas.MeasureCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id)
+    if db_user:
+        return crud.create_user_measure(db, measure, user_id)
+
+    
+
+#entrega uma lista de usuarios
+@app.get("/measures/", response_model=List[schemas.Measure])
+def read_measures(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_measures(db, skip=skip, limit=limit)
     return users
