91b0ab627dbb38d9d4c2e0e1a0307901ccc5f3a5
unknown <natalia.kaneshima@edu.unifil.br>
2021-12-01 03:12:28 -0300

    teste2

--- backend/backend/crud.py
+++ backend/backend/crud.py
@@ -1,61 +1,62 @@
 from sqlalchemy.orm import Session
 from . import models, schemas
-
-
+from sqlalchemy import update
+ 
+ 
 #Usuario
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
-
+ 
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
-
+ 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
-
-
+ 
+ 
 #Criar usuario
 def create_user(db: Session, user: schemas.UserCreate):
     fake_hashed_password = user.password + "notreallyhashed"
     db_user = models.User(
-        name=user.name, 
-        email=user.email, 
+        name=user.name,
+        email=user.email,
         hashed_password=fake_hashed_password
     )
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
-
-
+ 
+ 
 #ediçao_user
-def edicao_user(db: Session, user_id: int, new_value: schemas.UserEdit):
+def edi_user(db: Session, user_id: int, new_value: schemas.UserEdit):
     db_user = db.query(models.User).filter(models.User.id == user_id).first()
     if db_user:
-        if new_value.email != None: 
+        if new_value.email != None:
             db.query(models.User).filter(models.User.id == user_id).\
                 update({"email": new_value.email})
         if new_value.name != None:
             db.query(models.User).filter(models.User.id == user_id).\
                 update({"name": new_value.name})
-        if new_value.birth_date != None: 
+        if new_value.birth_date != None:
             db.query(models.User).filter(models.User.id == user_id).\
                 update({"birth_date": new_value.birth_date})
-
+ 
     db.commit()
     db.refresh(db_user)
     return db_user
-        
-
+       
+ 
 #Medidas
 def get_measures(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Measure).offset(skip).limit(limit).all()
-
-
+ 
+ 
 #Usuario medidas
 def get_user_measure(db: Session, user_id: int):
     return db.query(models.Measure).filter(models.Measure.user_id == user_id).all()
-
-
+ 
+ 
 #Criar medidas
 def create_user_measure(db: Session, measure: schemas.MeasureCreate, user_id: int):
     db_measure = models.Measure(**measure.dict(), owner_id=user_id)
@@ -63,3 +64,4 @@ def create_user_measure(db: Session, measure: schemas.MeasureCreate, user_id: in
     db.commit()
     db.refresh(db_measure)
     return db_measure
+ 
--- backend/backend/main.py
+++ backend/backend/main.py
@@ -1,63 +1,76 @@
 from typing import List
-
+ 
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
-
-
+ 
+ 
 from . import crud, models, schemas
 from .database import SessionLocal, engine
-
+ 
 models.Base.metadata.create_all(bind=engine)
-    
+   
 app = FastAPI()
-    
+   
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-
+ 
 #Aqui para baixo Usuario
-
-#cadastrar um usuario
+ 
+ 
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="E-mail já cadastrado")
-
+ 
     user = crud.create_user(db=db, user=user)
-    
+   
     return user
-
-
-#entrega uma lista de usuarios
+ 
+ 
 @app.get("/users/", response_model=List[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
-
-
-
+ 
+ 
+ 
 @app.get("/users/{user_id}", response_model=schemas.User)
-def get_user_id(user_id: int, db: Session = Depends(get_db)):
-    return crud.get_user(db, user_id)
-
-
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="Usuario não encontrado")
+    return db_user
+ 
+ 
+ 
 #Aqui para baixo Measure
-
-#cadastrar um usuario
+ 
 @app.post("/users/{user_id}/measures/", response_model=schemas.Measure)
 def create_measure(user_id: int, measure: schemas.MeasureCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id)
     if db_user:
         return crud.create_user_measure(db, measure, user_id)
-
-    
-
-#entrega uma lista de usuarios
+ 
+    raise HTTPException(status_code=400, detail="Usuario não encontrado")
+   
+ 
 @app.get("/measures/", response_model=List[schemas.Measure])
 def read_measures(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_measures(db, skip=skip, limit=limit)
     return users
+ 
+ 
+@app.get("/users/{user_id}/measures/", response_model=schemas.Measure)
+def user_measure(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id)
+    if db_user:
+        return crud.get_user_measure(db, user_id)
+ 
+    raise HTTPException(status_code=400, detail="Usuario não encontrado")
+   
+ 
